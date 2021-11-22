a3ef76cd0336824ccf33ee717830326e8cde64ed
com1lc <plato2h@gmail.com>
2021-11-22 16:59:39 +0700

    first program

--- HelloWord.py
+++ HelloWord.py
@@ -1,1 +1,5 @@
-print('Hello world')
+name = input("what is your name? : ")
+age = int(input("How old are you? : "))
+result = str((100 - age) + 2021)
+
+print( name + " will be 100 years old in the year " + result)
